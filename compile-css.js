a1ca1186628bc31bcc5471f2149e59d3c94263f5
Christian von Rohr <ch.vrohr@gmail.com>
2018-06-05 17:46:45 +0200

    [TASK] Refactor addon-css integration

---
+++ compile-css.js
@@ -0,0 +1,15 @@
+var sass = require('node-sass'); // eslint-disable-line
+var fs = require('fs');
+var path = require('path');
+
+var inputFile = path.join(__dirname, 'app', 'styles', 'ember-gantt.scss');
+var outputFile = path.join(__dirname, 'vendor', 'ember-gantt.css');
+var buf = fs.readFileSync(inputFile, "utf8");
+
+// Compile main file
+var result = sass.renderSync({
+  data: buf,
+  includePaths: ['app/styles']
+});
+
+fs.writeFileSync(outputFile, result.css);
