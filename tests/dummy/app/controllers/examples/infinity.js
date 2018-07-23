8eafdb196fdea41b35bd07cfc603f8cd8552dbf4
Christian von Rohr <ch.vrohr@gmail.com>
2018-07-23 12:03:40 +0200

    [FIX] eslint fixes

--- tests/dummy/app/controllers/examples/infinity.js
+++ tests/dummy/app/controllers/examples/infinity.js
@@ -1,17 +1,17 @@
 import Controller from '@ember/controller';
 import ExampleController from '../../mixins/example-controller';
-import {set} from '@ember/object';
-
-import dateUtil from '@kockpit/ember-gantt/utils/date-util';
-
 
 export default Controller.extend(ExampleController, {
 
   actions: {
 
     viewDateChange(startDate, endDate) {
+      /* eslint-disable */
+
       console.log(startDate, 'start view date changed');
       console.log(endDate, ' end view date changed');
+
+      /* eslint-enable */
     }
 
   }
